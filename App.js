0d9753bcc9d01e79784bc528ab2dbb1f1b2fe804
dsam5 <98347052+dsam5@users.noreply.github.com>
2023-07-12 22:11:09 -1200

    Initial commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+         <KeyboardAvoidingView 
+         behavior={Platform.OS ==="ios" ? "padding" : "height"}
+         style={{flex: 1}}
+         keyboardVerticalOffset={Platform.OS === "android" ? -65 : 0}
+         >
+        
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+            </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+}
